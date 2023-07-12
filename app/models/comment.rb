f535cc0edd1e79c269bcfa573aebe23af4228d5e
MarwanDev <marwaneg@aucegypt.edu>
2023-07-13 02:31:45 +0300

    Add data processing methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
-  belongs_to :author
-  belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, class_name: 'Post'
+  after_save :increment_comments_count
+
+  private
+
+  def increment_comments_count
+    post.increment!(:comments_counter)
+  end
 end
